b3d130cb08069da0f5e569ded89bcb4f53092c43
Václav Mareška <mareska.vaclav@centrum.cz>
2026-07-14 04:02:59 +0200

    Add CO2 measurement interval calculation based on battery percentage

--- include/meteo_station_common/battery.h
+++ include/meteo_station_common/battery.h
@@ -19,4 +19,31 @@ inline void setupBatteryPin(uint8_t pin) {
     analogSetPinAttenuation(pin, ADC_11db);
 }
 
+inline int co2MeasureCountsForBattery(double batteryPct,
+                                      int baseCounts,
+                                      int sleepMinutes,
+                                      int maxIntervalMinutes = 120) {
+    if (baseCounts < 1) {
+        baseCounts = 1;
+    }
+    if (sleepMinutes < 1) {
+        sleepMinutes = 1;
+    }
+
+    int intervalMinutes = baseCounts * sleepMinutes;
+    if (batteryPct < 60.0) {
+        int lowBatterySteps = 0;
+        for (double threshold = 60.0; threshold > 0.0 && batteryPct < threshold; threshold -= 5.0) {
+            ++lowBatterySteps;
+        }
+        intervalMinutes += lowBatterySteps * 10;
+    }
+
+    if (intervalMinutes > maxIntervalMinutes) {
+        intervalMinutes = maxIntervalMinutes;
+    }
+
+    return (intervalMinutes + sleepMinutes - 1) / sleepMinutes;
+}
+
 } // namespace meteo
